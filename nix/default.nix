0fd0a9c07f43636ce239f7f5f0242b48f0f6a038
David <ekstraplass11@gmail.com>
2025-12-02 19:17:39 +0100

    update old package name

--- nix/default.nix
+++ nix/default.nix
@@ -11,7 +11,7 @@
   gtk3,
   libpulseaudio,
   gjs,
-  wrapGAppsHook,
+  wrapGAppsHook3,
   upower,
   gnome-bluetooth,
   gtk-layer-shell,
@@ -22,10 +22,11 @@
   libsoup_3,
   libnotify,
   pam,
-  extraPackages ? [],
+  extraPackages ? [ ],
   version ? "git",
   buildTypes ? true,
-}: let
+}:
+let
   pname = "ags";
 
   gvc-src = fetchFromGitLab {
@@ -36,79 +37,84 @@
     sha256 = "sha256-FosJwgTCp6/EI6WVbJhPisokRBA6oT0eo7d+Ya7fFX8=";
   };
 in
-  stdenv.mkDerivation {
-    inherit pname version;
+stdenv.mkDerivation {
+  inherit pname version;
 
-    src = buildNpmPackage {
-      name = pname;
+  src = buildNpmPackage {
+    name = pname;
 
-      src = builtins.path {
-        name = "ags-${version}";
-        path = lib.cleanSource ../.;
-      };
-
-      dontBuild = true;
-
-      npmDepsHash = "sha256-ucWdADdMqAdLXQYKGOXHNRNM9bhjKX4vkMcQ8q/GZ20=";
-
-      installPhase = ''
-        runHook preInstall
-        mkdir $out
-        cp -r * $out
-        runHook postInstall
-      '';
+    src = builtins.path {
+      name = "ags-${version}";
+      path = lib.cleanSource ../.;
     };
 
-    nativeBuildInputs = [
-      pkg-config
-      meson
-      ninja
-      nodePackages.typescript
-      wrapGAppsHook
-      gobject-introspection
-    ];
+    dontBuild = true;
 
-    buildInputs =
-      [
-        gjs
-        gtk3
-        libpulseaudio
-        upower
-        gnome-bluetooth
-        gtk-layer-shell
-        glib-networking
-        networkmanager
-        libdbusmenu-gtk3
-        gvfs
-        libsoup_3
-        libnotify
-        pam
-      ]
-      ++ extraPackages;
+    npmDepsHash = "sha256-ucWdADdMqAdLXQYKGOXHNRNM9bhjKX4vkMcQ8q/GZ20=";
 
-    mesonFlags = [
-      (lib.mesonBool "build_types" buildTypes)
-    ];
-
-    prePatch = ''
-      mkdir -p ./subprojects/gvc
-      cp -r ${gvc-src}/* ./subprojects/gvc
+    installPhase = ''
+      runHook preInstall
+      mkdir $out
+      cp -r * $out
+      runHook postInstall
     '';
+  };
 
-    postPatch = ''
-      chmod +x post_install.sh
-      patchShebangs post_install.sh
-    '';
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    ninja
+    nodePackages.typescript
+    wrapGAppsHook3
+    gobject-introspection
+  ];
 
-    outputs = ["out" "lib"];
+  buildInputs = [
+    gjs
+    gtk3
+    libpulseaudio
+    upower
+    gnome-bluetooth
+    gtk-layer-shell
+    glib-networking
+    networkmanager
+    libdbusmenu-gtk3
+    gvfs
+    libsoup_3
+    libnotify
+    pam
+  ]
+  ++ extraPackages;
 
-    meta = {
-      description = "A customizable and extensible shell";
-      homepage = "https://github.com/Aylur/ags";
-      changelog = "https://github.com/Aylur/ags/blob/${version}/CHANGELOG.md";
-      platforms = ["x86_64-linux" "aarch64-linux"];
-      license = lib.licenses.gpl3;
-      mainProgram = "ags";
-      maintainers = [lib.maintainers.Aylur];
-    };
-  }
+  mesonFlags = [
+    (lib.mesonBool "build_types" buildTypes)
+  ];
+
+  prePatch = ''
+    mkdir -p ./subprojects/gvc
+    cp -r ${gvc-src}/* ./subprojects/gvc
+  '';
+
+  postPatch = ''
+    chmod +x post_install.sh
+    patchShebangs post_install.sh
+  '';
+
+  outputs = [
+    "out"
+    "lib"
+  ];
+
+  meta = {
+    description = "A customizable and extensible shell";
+    homepage = "https://github.com/Aylur/ags";
+    changelog = "https://github.com/Aylur/ags/blob/${version}/CHANGELOG.md";
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    license = lib.licenses.gpl3;
+    mainProgram = "ags";
+    maintainers = [ lib.maintainers.Aylur ];
+  };
+}
